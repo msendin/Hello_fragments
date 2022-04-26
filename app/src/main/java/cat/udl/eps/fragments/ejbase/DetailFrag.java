e47adc8c89c6ee3bf7dc64d7c91e9f9b0305a0e1
Montse <msendinv@gmail.com>
2022-04-26 17:27:41 +0200

    Giving all methods goals

--- app/src/main/java/cat/udl/eps/fragments/ejbase/DetailFrag.java
+++ app/src/main/java/cat/udl/eps/fragments/ejbase/DetailFrag.java
@@ -2,31 +2,30 @@ package cat.udl.eps.fragments.ejbase;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import java.util.Objects;
+import androidx.fragment.app.Fragment;
 
 public class DetailFrag extends Fragment{
-
-
-	@Override
-	public void onViewCreated(@NonNull View v, Bundle savedInstanceState) {
-		super.onViewCreated(v, savedInstanceState);
-	}
+	TextView view;
 
 	@Override
-	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+							 @Nullable Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
 		return inflater.inflate(R.layout.detail_fragment, container, false);
 	}
 
+	@Override
+	public void onViewCreated(@NonNull View v, Bundle savedInstanceState) {
+		super.onViewCreated(v, savedInstanceState);
+		view = requireView().findViewById(R.id.captain);
+	}
+
 	public void showText(String item) {
-		TextView view = requireView().findViewById(R.id.captain);
 		view.setText(item);
 	}
 }
